c44d18bdd2654cead49aa1f57fbc3d6c35e3cb1a
“Spencer <Spencer@bookservo.com>
2023-09-12 17:14:26 -0400

    began piece class

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -2,16 +2,16 @@ require_relative 'piece'
 
 class Board
     def initialize
-        @board = Array.new(2) {Array.new(8,Piece.new)} + 
+        @board = Array.new(2) {Array.new(8, Piece.new([0,0], self, :W))} + 
         Array.new(4) {Array.new(8, nil)} +  
-        Array.new(2) {Array.new(8,Piece.new)}
+        Array.new(2) {Array.new(8, Piece.new([0,0], self, :B))}
     end
 
-    def print
-        @board.each do |row|
-            puts row.join(' ')
-        end
-    end
+    # def print
+    #     @board.each do |row|
+    #         puts row.join(' ')
+    #     end
+    # end
 
     def [](pos)
         row,col = pos
@@ -26,10 +26,12 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
-
         raise 'no piece at start pos' unless self[start_pos].is_a?(Piece)
-        
-
         raise 'invalid end pos' if end_pos.any? {|pos| !pos.between?(0,7)}
+
+        self[end_pos] = self[start_pos]
+        self[start_pos] = nil
     end
+
+    attr_accessor :board
 end
--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,5 +1,17 @@
 class Piece
+    attr_reader :board
+
+    def initialize(pos, board, color)
+        @pos = pos
+        @color = color
+        @board = board
+    end
+
+    # def empty?
+    #     self == nullpiece
+    # end
+
     def inspect
-        'P'
+        'P' 
     end
 end
